aef1aa7d119ac96bea58126d77c63fba3cebaf12
DanielG <Danielmorrisg@gmail.com>
2018-05-28 16:41:57 -0700

    added initial JS notes to get started

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Wrapper document.ready function to only load JavaScript after HTML is ready
+// Function to select a random number at the start of the game
+// Append that number to the proper DIV
+// Select random numbers for each piece of gold between 1-12
+// Set up variables for wins/losses/totals
+// Add a function to reset the game
+// Add a win function
+// Add a lose functioN
+// Add on click functions to the gold pieces
+
+$(document).ready(function(){
+    
+});
