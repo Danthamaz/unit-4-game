fbdb7ede40244e238cdfc2a84de7982162662848
DanielG <Danielmorrisg@gmail.com>
2018-05-28 17:40:13 -0700

    Added random number generator for number to try to get

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-// Wrapper document.ready function to only load JavaScript after HTML is ready
-// Function to select a random number at the start of the game
-// Append that number to the proper DIV
+// Wrapper document.ready function to only load JavaScript after HTML is ready X
+// Function to select a random number at the start of the game X
+// Append that number to the proper DIV X 
 // Select random numbers for each piece of gold between 1-12
 // Set up variables for wins/losses/totals
 // Add a function to reset the game
@@ -8,6 +8,13 @@
 // Add a lose functioN
 // Add on click functions to the gold pieces
 
+// Wrapper function to only load JavaScript after page loads
 $(document).ready(function(){
+
+    // Generates random number between 19-120 and appends it to the DIV #randNumber
+    var selectedNumber = Math.floor(Math.random() * 102) + 19;
+    $("#randNumber").append(selectedNumber);
+
     
+
 });
